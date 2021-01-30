068491c2baebf3c701e1f330c6fe034bde8ecc9c
Kieran Lewin <afrenchrussian@gmail.com>
2021-01-30 18:37:49 +0000

    added tile

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 
 export { default as Button } from './components/Button';
 export { default as Tickbox } from './components/Tickbox';
+export { default as Tile } from './components/TileList/Tile';
 export { default as TileList } from './components/TileList/TileList';
 export { default as TextBox } from './components/TextBox';
 export { Lock, Room, Mail, Key } from './components/Icons';
