9990169d04155c560ffb031e9a20bf159d03893d
Kieran Lewin <afrenchrussian@gmail.com>
2021-02-01 22:47:55 +0000

    fixed imports for Github icon

--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,4 @@ export { default as Tickbox } from './components/Tickbox';
 export { default as Tile } from './components/TileList/Tile';
 export { default as TileList } from './components/TileList/TileList';
 export { default as TextBox } from './components/TextBox';
-export { Lock, Room, Mail, Key, Google } from './components/Icons';
+export { Lock, Room, Mail, Key, Google, Github } from './components/Icons';
