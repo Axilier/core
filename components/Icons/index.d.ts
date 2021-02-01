d45810871f725644a1c6b562d0fe8998dc6c0e5c
KLewin23 <afrenchrussian@gmail.com>
2021-02-01 22:49:40 +0000

    Deploying to build from  @ 9ab9ef96d0a8a4f951fd980b5d8d432424f5670d 🚀

--- components/Icons/index.d.ts
+++ components/Icons/index.d.ts
@@ -4,3 +4,4 @@ export { default as Room } from './Room';
 export { default as Mail } from './Mail';
 export { default as Key } from './Key';
 export { default as Google } from './Google';
+export { default as Github } from './Github';
