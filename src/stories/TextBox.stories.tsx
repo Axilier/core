d4b18768a864bbffd1450e5d5aaee459bcb7bac8
Kieran Lewin <afrenchrussian@gmail.com>
2021-02-04 22:24:58 +0000

    added bare variant

--- src/stories/TextBox.stories.tsx
+++ src/stories/TextBox.stories.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { TextBoxProps } from '../Types';
-import { Mail, TextBox } from '../index';
+import { Mail, Search, TextBox } from '../index';
 
 export default {
     title: 'Example/TextBox',
@@ -26,6 +26,38 @@ Outlined.args = {
     size: 'large',
     label: 'Password',
     required: true,
+};
+
+export const Bare = Template.bind({});
+Bare.args = {
+    variant: 'bare',
+    placeholder: 'Test placeholder',
+    size: 'large',
+    required: true,
+    prefixComponent: <Search iconColor={'#8C8C8C'} />,
+};
+
+export const ColouredOutline = Template.bind({});
+ColouredOutline.args = {
+    variant: 'outlined',
+    placeholder: 'Test placeholder',
+    size: 'large',
+    label: 'Password',
+    required: true,
+    outLineColor: 'red',
+};
+
+export const ChangedFont = Template.bind({});
+ChangedFont.args = {
+    variant: 'bare',
+    placeholder: 'Test placeholder',
+    size: 'large',
+    required: true,
+    prefixComponent: <Search iconColor={'#8C8C8C'} />,
+    inputStyle: {
+        fontWeight: 500,
+        color: 'red',
+    },
 };
 
 export const NumbersOnly = Template.bind({});
